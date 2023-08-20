8e0cea18fe74f007521425867942e2742f5a00da
Yevhenns <gifestum@gmail.com>
2023-08-20 15:25:32 +0300

    stack

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
 import LoginScreen from "./Screens/LoginScreen";
-// import RegistrationScreen from "./Screens/RegistrationScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const AuthStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
-    </>
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen name="register" component={RegistrationScreen} />
+        <AuthStack.Screen name="login" component={LoginScreen} />
+      </AuthStack.Navigator>
+    </NavigationContainer>
   );
 }
