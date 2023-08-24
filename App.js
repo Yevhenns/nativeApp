29f3a47c5a565cc56153b5770467167ca43896d2
Yevhenns <gifestum@gmail.com>
2023-08-24 14:38:18 +0300

    routing

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import RegistrationScreen from "./Screens/Auth/RegistrationScreen";
 import LoginScreen from "./Screens/Auth/LoginScreen";
+import Home from "./Screens/Main/Home";
+import PostsScreen from "./Screens/Main/PostsScreen";
+import CreatePostsScreen from "./Screens/Main/CreatePostsScreen";
+import ProfileScreen from "./Screens/Main/ProfileScreen";
 
 const AuthStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
       <AuthStack.Navigator>
         <AuthStack.Screen
           options={{ headerShown: false }}
@@ -22,6 +26,19 @@ export default function App() {
           component={LoginScreen}
         />
       </AuthStack.Navigator>
-    </NavigationContainer>
+    );
+  }
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Posts Screen" component={PostsScreen} />
+      <Tab.Screen name="Create Posts Screen" component={CreatePostsScreen} />
+      <Tab.Screen name="Profile Screen" component={ProfileScreen} />
+    </Tab.Navigator>
   );
+};
+
+export default function App() {
+  const routing = useRoute(true);
+  return <NavigationContainer>{routing}</NavigationContainer>;
 }
